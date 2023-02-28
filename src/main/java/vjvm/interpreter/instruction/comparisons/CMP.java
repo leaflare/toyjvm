a1e9cd552608bd40b11366c4e76c3d887e78f099
fangli <fangli10248@outlook.com>
2023-02-28 23:45:23 +0800

    Lab2 v1.2.2 Updated interpreter framework

---
+++ src/main/java/vjvm/interpreter/instruction/comparisons/CMP.java
@@ -0,0 +1,73 @@
+package vjvm.interpreter.instruction.comparisons;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.var;
+import vjvm.interpreter.instruction.Instruction;
+import vjvm.runtime.JThread;
+import vjvm.runtime.classdata.MethodInfo;
+import vjvm.runtime.frame.OperandStack;
+import vjvm.runtime.frame.ProgramCounter;
+
+import java.util.function.Function;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CMP<T extends Number> extends Instruction {
+
+    private final Function<OperandStack, T> popFunc;
+    private final boolean true4NaN;
+    private final String name;
+
+    public static CMP<Long> LCMP(ProgramCounter pc, MethodInfo method) {
+        return new CMP<>(OperandStack::popLong, false, "lcmp");
+    }
+
+    public static CMP<Float> FCMPL(ProgramCounter pc, MethodInfo method) {
+        return new CMP<>(OperandStack::popFloat, false, "fcmpl");
+    }
+
+    public static CMP<Float> FCMPG(ProgramCounter pc, MethodInfo method) {
+        return new CMP<>(OperandStack::popFloat, true, "fcmpg");
+    }
+
+    public static CMP<Double> DCMPL(ProgramCounter pc, MethodInfo method) {
+        return new CMP<>(OperandStack::popDouble, false, "dcmpl");
+    }
+
+    public static CMP<Double> DCMPG(ProgramCounter pc, MethodInfo method) {
+        return new CMP<>(OperandStack::popDouble, true, "dcmpg");
+    }
+
+
+
+    @Override
+    public void run(JThread thread) {
+        var stack = thread.top().stack();
+        T value2 = popFunc.apply(stack);
+        T value1 = popFunc.apply(stack);
+
+        if (Double.isNaN(value1.doubleValue()) || Double.isNaN(value2.doubleValue())) {
+            if (true4NaN) {
+                stack.pushInt(1);
+            } else {
+                stack.pushInt(-1);
+            }
+        } else {
+            if (value1.doubleValue() > value2.doubleValue()) {
+                stack.pushInt(1);
+            } else if (value1.doubleValue() == value2.doubleValue()) {
+                stack.pushInt(0);
+            } else if (value1.doubleValue() < value2.doubleValue()){
+                stack.pushInt(-1);
+            } else {
+                assert false;
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/vjvm/interpreter/instruction/math/INC.java
@@ -0,0 +1,36 @@
+package vjvm.interpreter.instruction.math;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.var;
+import vjvm.interpreter.instruction.Instruction;
+import vjvm.runtime.JThread;
+import vjvm.runtime.classdata.MethodInfo;
+import vjvm.runtime.frame.ProgramCounter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+
+public class INC extends Instruction {
+
+    private final ProgramCounter pc;
+
+    public static INC IINC(ProgramCounter pc, MethodInfo method) {
+        return new INC(pc);
+    }
+
+    @Override
+    public void run(JThread thread) {
+
+        var slots = thread.top().vars();
+
+        int index = pc.ubyte();
+        int value = pc.byte_();
+
+        slots.int_(index, slots.int_(index) + value);
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
