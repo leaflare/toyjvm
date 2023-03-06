43847513b5f5e1ad00521f7e5479c15ce80106b1
fangli <fangli10248@outlook.com>
2023-03-06 21:05:16 +0800

    Lab2 v1.2.3 Updated runtime framework

---
+++ src/main/java/vjvm/runtime/frame/OperandStack.java
@@ -0,0 +1,102 @@
+package vjvm.runtime.frame;
+
+import lombok.Getter;
+
+public class OperandStack {
+    @Getter
+    private final Slots slots;
+
+    @Getter
+    private int top;
+
+    public OperandStack(int stackSize) {
+        slots = new Slots(stackSize);
+        top = 0;
+    }
+
+    public void pushInt(int value) {
+        slots.int_(top, value);
+        top += 1;
+    }
+
+    public int popInt() {
+        top -= 1;
+        return slots.int_(top);
+    }
+
+    public void pushFloat(float value) {
+        slots.float_(top, value);
+        top += 1;
+    }
+
+    public float popFloat() {
+        top -= 1;
+        return slots.float_(top);
+    }
+
+    public void pushLong(long value) {
+        slots.long_(top, value);
+        top += 2;
+    }
+
+    public long popLong() {
+        top -= 2;
+        return slots.long_(top);
+    }
+
+    public void pushDouble(double value) {
+        slots.double_(top, value);
+        top += 2;
+    }
+
+    public double popDouble() {
+        top -= 2;
+        return slots.double_(top);
+    }
+
+    public void pushByte(byte value) {
+        slots.byte_(top, value);
+        top += 1;
+    }
+
+    public byte popByte() {
+        top -= 1;
+        return slots.byte_(top);
+    }
+
+    public void pushChar(char value) {
+        slots.char_(top, value);
+        top += 1;
+    }
+
+    public char popChar() {
+        top -= 1;
+        return slots.char_(top);
+    }
+
+    public void pushShort(short value) {
+        slots.short_(top, value);
+        top += 1;
+    }
+
+    public short popShort() {
+        top -= 1;
+        return slots.short_(top);
+    }
+
+    public void pushSlots(Slots slots) {
+        slots.copyTo(0, slots.size(), this.slots, top);
+        top += slots.size();
+    }
+
+    public Slots popSlots(int count) {
+        Slots slots1 = new Slots(count);
+        top -= count;
+        slots.copyTo(top, count, slots1, 0);
+        return slots1;
+    }
+
+    public void clear() {
+        top = 0;
+    }
+}
---
+++ src/main/java/vjvm/runtime/frame/Slots.java
@@ -0,0 +1,90 @@
+package vjvm.runtime.frame;
+
+import java.util.Optional;
+
+/**
+ * Slots represents an array of JVM slots as defined in the specification. It
+ * supports getting and putting primitive data types, including address.
+ */
+public class Slots {
+
+    public Object[] slots;
+
+    public Slots(int slotSize) {
+        slots = new Object[slotSize];
+    }
+
+    public int int_(int index) {
+        return (int) slots[index];
+    }
+
+    public void int_(int index, int value) {
+        slots[index] = value;
+    }
+
+    public long long_(int index) {
+        return (long) slots[index];
+    }
+
+    public void long_(int index, long value) {
+        slots[index] = value;
+    }
+
+    public float float_(int index) {
+        return (float) slots[index];
+    }
+
+    public void float_(int index, float value) {
+        slots[index] = value;
+    }
+
+    public double double_(int index) {
+        return (double) slots[index];
+    }
+
+    public void double_(int index, double value) {
+        slots[index] = value;
+    }
+
+    public byte byte_(int index) {
+        return (byte) slots[index];
+    }
+
+    public void byte_(int index, byte value) {
+        slots[index] = (int) value;
+    }
+
+    public char char_(int index) {
+        return (char) (int) slots[index];
+    }
+
+    public void char_(int index, char value) {
+        slots[index] = (int) value;
+    }
+
+    public short short_(int index) {
+        return (short) slots[index];
+    }
+
+    public void short_(int index, short value) {
+        slots[index] = (int) value;
+    }
+
+    public Optional<Object> value(int index) {
+        if (slots[index] != null) {
+            return Optional.of(slots[index]);
+        }
+        return Optional.empty();
+    }
+
+    public int size() {
+        return slots.length;
+    }
+
+    public void copyTo(int begin, int length, Slots dest, int destBegin) {
+        for (int i = begin; i < begin + length; i++) {
+            dest.slots[destBegin + i - begin] = this.slots[i];
+        }
+    }
+
+}
