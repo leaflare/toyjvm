7b2b926cd4d7ac566bf37c9d5e704c3773cef561
fangli <fangli10248@outlook.com>
2023-04-22 16:39:34 +0800

    Lab4 v1.4.2 Added Exception Handling

--- src/main/java/vjvm/classfiledefs/MethodDescriptors.java
+++ src/main/java/vjvm/classfiledefs/MethodDescriptors.java
@@ -1,58 +1,63 @@
 package vjvm.classfiledefs;
 
 import lombok.var;
+import vjvm.util.Logger;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MethodDescriptors {
-    public static int argc(String descriptor) {
-        assert descriptor.startsWith("(");
+  public static int argc(String descriptor) {
+    assert descriptor.startsWith("(");
 
-        List<Character> arr = descriptor.chars().mapToObj(i -> (char) i).collect(Collectors.toList());
+    List<Character> arr = descriptor.chars().mapToObj(i -> (char) i).collect(Collectors.toList());
 
-        int size = 0;
+    int size = 0;
 
-        while (arr.get(0) != ')') {
-            switch (arr.get(0)) {
-                case '(':
-                    break;
-                case 'J':  // Long
-                case 'D':  // Double
-                    size += 2;
-                    break;
-                case 'L':  // Class reference
-                    while (arr.get(0) != ';') {
-                        arr.remove(0);
-                    }
-                    size += 1;
-                case '[':  // Array reference
-                    while (arr.get(0) == '[') {
-                        arr.remove(0);
-                    }
-                    size += 1;
-                case 'B':  // Byte
-                case 'C':  // Char
-                case 'F':  // Float
-                case 'I':  // Int
-                case 'S':  // Short
-                case 'Z':  // Boolean
-                    size += 1;
-                    break;
-                default:
-                    assert false;
-            }
+    while (arr.get(0) != ')') {
+      switch (arr.get(0)) {
+        case '(':
+          break;
+        case Descriptors.DESC_long:
+        case Descriptors.DESC_double:
+          size += 2;
+          break;
+        case Descriptors.DESC_reference:
+          while (arr.get(0) != ';') {
             arr.remove(0);
-        }
-
-        return size;
+          }
+          size += 1;
+          break;
+        case Descriptors.DESC_array:  // Array reference
+          while (arr.get(0) == Descriptors.DESC_array) {
+            arr.remove(0);
+          }
+          size += 1;
+          break;
+        case Descriptors.DESC_byte:
+        case Descriptors.DESC_char:
+        case Descriptors.DESC_float:
+        case Descriptors.DESC_int:
+        case Descriptors.DESC_short:
+        case Descriptors.DESC_boolean:
+          size += 1;
+          break;
+        default:
+          assert false;
+      }
+      arr.remove(0);
     }
 
-    public static char returnType(String descriptor) {
-        assert descriptor.startsWith("(");
+    Logger.debug(descriptor + ", count: " + size);
 
-        var i = descriptor.indexOf(')') + 1;
-        assert i < descriptor.length();
-        return descriptor.charAt(i);
-    }
+    return size;
+  }
+
+  public static char returnType(String descriptor) {
+    assert descriptor.startsWith("(");
+
+    var i = descriptor.indexOf(')') + 1;
+    assert i < descriptor.length();
+    return descriptor.charAt(i);
+  }
 }
--- src/main/java/vjvm/interpreter/instruction/references/CHECKCAST.java
+++ src/main/java/vjvm/interpreter/instruction/references/CHECKCAST.java
@@ -1,23 +1,46 @@
 package vjvm.interpreter.instruction.references;
 
+import lombok.var;
 import vjvm.interpreter.instruction.Instruction;
 import vjvm.runtime.JThread;
+import vjvm.runtime.class_.ConstantPool;
 import vjvm.runtime.class_.MethodInfo;
 import vjvm.runtime.ProgramCounter;
+import vjvm.runtime.class_.constant.ClassInfoConstant;
+import vjvm.runtime.exception.JThrowable;
+import vjvm.runtime.reference.ObjectReference;
 
 public class CHECKCAST extends Instruction {
 
-    public CHECKCAST(ProgramCounter pc, MethodInfo methodInfo) {
+  int classInfoIndex;
 
+  ConstantPool cp;
+
+  public CHECKCAST(ProgramCounter pc, MethodInfo method) {
+    this.classInfoIndex = pc.ushort();
+    this.cp = method.jClass().constantPool();
+  }
+
+  @Override
+  public void run(JThread thread) {
+
+    var stack = thread.top().stack();
+    var ref = (ObjectReference) stack.popReference();
+
+    if (ref != ObjectReference.NULL) {
+      var classInfo = (ClassInfoConstant) cp.constant(classInfoIndex);
+      if (!ref.jClass().isSubclassOf(classInfo.getJClass())) {
+        var exception = thread.context().heap().objAlloc(thread.context().bootstrapLoader().loadClass("Ljava/lang/ClassCastException;"));
+        throw new JThrowable(exception);
+      }
     }
 
-    @Override
-    public void run(JThread thread) {
-        // TODO
-    }
+    // the stack is unchanged
+    stack.pushReference(ref);
+  }
 
-    @Override
-    public String toString() {
-        return "checkcast";
-    }
+  @Override
+  public String toString() {
+    return "checkcast";
+  }
 }
