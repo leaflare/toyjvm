1f09d0b36e88c63677b75488e2dbdcdf0b575962
fangli <fangli10248@outlook.com>
2023-04-30 22:42:11 +0800

    Lab4 v1.4.4 Added ATHROW & INSTANCEOF reference

--- src/main/java/vjvm/interpreter/instruction/control/RETURN.java
+++ src/main/java/vjvm/interpreter/instruction/control/RETURN.java
@@ -8,49 +8,54 @@ import vjvm.runtime.JThread;
 import vjvm.runtime.frame.OperandStack;
 import vjvm.runtime.ProgramCounter;
 import vjvm.runtime.class_.MethodInfo;
+import vjvm.runtime.reference.Reference;
 
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class RETURN<T> extends Instruction {
-    private final Function<OperandStack, T> popFunc;
-    private final Function<T, T> convertFunc;
-    private final BiConsumer<OperandStack, T> pushFunc;
-    private final String name;
+  private final Function<OperandStack, T> popFunc;
+  private final Function<T, T> convertFunc;
+  private final BiConsumer<OperandStack, T> pushFunc;
+  private final String name;
 
-    public static RETURN<Void> RETURN(ProgramCounter pc, MethodInfo method) {
-        return new RETURN<>(s -> null, Function.identity(), null, "return");
+  public static RETURN<Void> RETURN(ProgramCounter pc, MethodInfo method) {
+    return new RETURN<>(s -> null, Function.identity(), null, "return");
+  }
+
+  public static RETURN<Integer> IRETURN(ProgramCounter pc, MethodInfo method) {
+    return new RETURN<>(OperandStack::popInt, Function.identity(), OperandStack::pushInt, "ireturn");
+  }
+
+  public static RETURN<Long> LRETURN(ProgramCounter pc, MethodInfo method) {
+    return new RETURN<>(OperandStack::popLong, Function.identity(), OperandStack::pushLong, "lreturn");
+  }
+
+  public static RETURN<Float> FRETURN(ProgramCounter pc, MethodInfo method) {
+    return new RETURN<>(OperandStack::popFloat, Function.identity(), OperandStack::pushFloat, "freturn");
+  }
+
+  public static RETURN<Double> DRETURN(ProgramCounter pc, MethodInfo method) {
+    return new RETURN<>(OperandStack::popDouble, Function.identity(), OperandStack::pushDouble, "dreturn");
+  }
+
+  public static RETURN<Reference> ARETURN(ProgramCounter pc, MethodInfo method) {
+    return new RETURN<>(OperandStack::popReference, Function.identity(), OperandStack::pushReference, "areturn");
+  }
+
+  @Override
+  public void run(JThread thread) {
+    var returnValue = popFunc.apply(thread.top().stack());
+    thread.pop();
+
+    if (pushFunc != null) {
+      pushFunc.accept(thread.top().stack(), convertFunc.apply(returnValue));
     }
+  }
 
-    public static RETURN<Integer> IRETURN(ProgramCounter pc, MethodInfo method) {
-        return new RETURN<>(OperandStack::popInt, Function.identity(), OperandStack::pushInt, "ireturn");
-    }
-
-    public static RETURN<Long> LRETURN(ProgramCounter pc, MethodInfo method) {
-        return new RETURN<>(OperandStack::popLong, Function.identity(), OperandStack::pushLong, "lreturn");
-    }
-
-    public static RETURN<Float> FRETURN(ProgramCounter pc, MethodInfo method) {
-        return new RETURN<>(OperandStack::popFloat, Function.identity(), OperandStack::pushFloat, "freturn");
-    }
-
-    public static RETURN<Double> DRETURN(ProgramCounter pc, MethodInfo method) {
-        return new RETURN<>(OperandStack::popDouble, Function.identity(), OperandStack::pushDouble, "dreturn");
-    }
-
-    @Override
-    public void run(JThread thread) {
-        var returnValue = popFunc.apply(thread.top().stack());
-        thread.pop();
-
-        if (pushFunc != null) {
-            pushFunc.accept(thread.top().stack(), convertFunc.apply(returnValue));
-        }
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
+  @Override
+  public String toString() {
+    return name;
+  }
 }
--- src/main/java/vjvm/interpreter/instruction/references/NEW.java
+++ src/main/java/vjvm/interpreter/instruction/references/NEW.java
@@ -3,31 +3,31 @@ package vjvm.interpreter.instruction.references;
 import lombok.var;
 import vjvm.interpreter.instruction.Instruction;
 import vjvm.runtime.JThread;
+import vjvm.runtime.ProgramCounter;
 import vjvm.runtime.class_.MethodInfo;
 import vjvm.runtime.class_.constant.ClassInfoConstant;
-import vjvm.runtime.ProgramCounter;
 
 public class NEW extends Instruction {
   /**
    * new an Object
    */
 
-  private final ClassInfoConstant classInfo;
+    private final ClassInfoConstant classInfo;
 
-  public NEW(ProgramCounter pc, MethodInfo method) {
-    var index = pc.ushort();
-    this.classInfo = (ClassInfoConstant) method.jClass().constantPool().constant(index);
-  }
+    public NEW(ProgramCounter pc, MethodInfo method) {
+        var index = pc.ushort();
+        this.classInfo = (ClassInfoConstant) method.jClass().constantPool().constant(index);
+    }
 
-  @Override
-  public void run(JThread thread) {
-    var stack = thread.top().stack();
-    classInfo.getJClass().init(thread);
-    stack.pushReference(thread.context().heap().objAlloc(classInfo.getJClass()));
-  }
+    @Override
+    public void run(JThread thread) {
+        var stack = thread.top().stack();
+        classInfo.getJClass().init(thread);
+        stack.pushReference(thread.context().heap().objAlloc(classInfo.getJClass()));
+    }
 
-  @Override
-  public String toString() {
-    return "new " + classInfo.name();
-  }
+    @Override
+    public String toString() {
+        return "new " + classInfo.name();
+    }
 }
